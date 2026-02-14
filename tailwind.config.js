efde70231c94ea5f23af65db6ca9d83cc2cc03b4
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2026-02-14 14:09:44 -0800

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      base: '16px',
+      xs: '12px',
+      sm: '14px',
+      md: '16px',
+      lg: '18px',
+      xl: '20px',
+    },
+    extend: {
+      fontSize: {
+        base: '16px',
+        xs: '14px',
+        sm: '14px',
+        md: '16px',
+        lg: '18px',
+        xl: '20px',
+        '2xl': '24px',
+        '3xl': '32px',
+        '4xl': '40px',
+        '5xl': '48px',
+        '6xl': '56px',
+        '7xl': '64px',
+        '8xl': '72px',
+        '9xl': '80px',
+        '10xl': '88px',
+      },
+    },
   },
   plugins: [],
-}
+};
